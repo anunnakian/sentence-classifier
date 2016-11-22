1e6f435eacc679809d6f3cf0a276e72d3a24721e
Mohamed <mohamed.aitabderrahman@gmail.com>
2016-11-22 23:25:12 +0000

    Calculate Mean & Standard deviation

--- classifier.py
+++ classifier.py
@@ -39,6 +39,22 @@ def separate_by_class(data_set):
     return separated
 
 
+" Calculate Mean "
+
+
+def mean(numbers):
+    return sum(numbers) / float(len(numbers))
+
+
+" Calculate Standard Deviation "
+
+
+def sd(numbers):
+    avg = mean(numbers)
+    variance = sum([pow(x - avg, 2) for x in numbers]) / float(len(numbers) - 1)
+    return math.sqrt(variance)
+
+
 if __name__ == '__main__':
     filename = "./input.csv";
     ds = load_csv(filename)
